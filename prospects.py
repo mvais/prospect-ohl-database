6f0c19881e676b9028e7385b9e5257d3d5c25959
Vais Mahendran <vais.mahendran@gmail.com>
2020-06-30 17:08:47 -0400

    Update ProspectsAPI to support other leagues

--- prospects.py
+++ prospects.py
@@ -1,11 +1,39 @@
 import requests
 
 class ProspectsAPI:
-    __url = 'https://lscluster.hockeytech.com/feed/?client_code=ohl&key=2976319eb44abe94'
+    __current = 'ohl'
+    __leagues = {
+        'ohl': {
+            'code': 'ohl',
+            'season_ids': [68],
+            'key': '2976319eb44abe94'
+        },
+        'whl': {
+            'code': 'whl',
+            'season_ids': [270],
+            'key': '41b145a848f4bd67'
+        },
+        'qmjhl': {
+            'code': 'lhjmq',
+            'season_ids': [193],
+            'key': 'f322673b6bcae299'
+        },
+    }
+
+    @classmethod
+    def __url(cls):
+        data = cls.__leagues[cls.__current]
+
+        return f"https://lscluster.hockeytech.com/feed/?client_code={data['code']}&key={data['key']}"
+
+    @classmethod
+    def set_league(cls, league):
+        if league in cls.__leagues:
+            cls.__current = league
 
     @classmethod
     def teams(cls):
-        response = requests.get(f'{cls.__url}&feed=modulekit&view=teamsbyseason')
+        response = requests.get(f'{cls.__url()()}&feed=modulekit&view=teamsbyseason')
 
         if response.status_code == 200:
             return response.json()['SiteKit']['Teamsbyseason']
@@ -22,7 +50,7 @@ class ProspectsAPI:
 
     @classmethod
     def player_profile(cls, player_id):
-        response = requests.get(f'{cls.__url}&feed=modulekit&view=player&category=profile&player_id={player_id}')
+        response = requests.get(f'{cls.__url()}&feed=modulekit&view=player&category=profile&player_id={player_id}')
 
         if response.status_code == 200:
             return response.json()['SiteKit']['Player']
@@ -31,7 +59,7 @@ class ProspectsAPI:
 
     @classmethod
     def player_game_by_game_stats(cls, player_id):
-        response = requests.get(f'{cls.__url}&feed=modulekit&view=player&category=gamebygame&player_id={player_id}')
+        response = requests.get(f'{cls.__url()}&feed=modulekit&view=player&category=gamebygame&player_id={player_id}')
 
         if response.status_code == 200:
             return response.json()['SiteKit']['Player']
@@ -40,7 +68,7 @@ class ProspectsAPI:
 
     @classmethod
     def player_stats(cls, player_id):
-        response = requests.get(f'{cls.__url}&feed=modulekit&view=player&category=seasonstats&player_id={player_id}')
+        response = requests.get(f'{cls.__url()}&feed=modulekit&view=player&category=seasonstats&player_id={player_id}')
 
         if response.status_code == 200:
             return response.json()['SiteKit']['Player']
@@ -49,7 +77,7 @@ class ProspectsAPI:
 
     @classmethod
     def schedule(cls):
-        response = requests.get(f'{cls.__url}&feed=modulekit&view=schedule')
+        response = requests.get(f'{cls.__url()}&feed=modulekit&view=schedule')
 
         if response.status_code == 200:
             return response.json()['SiteKit']['Schedule']
@@ -65,7 +93,7 @@ class ProspectsAPI:
 
     @classmethod
     def game_summary(cls, game_id):
-        response = requests.get(f'{cls.__url}&feed=gc&game_id={game_id}&tab=gamesummary')
+        response = requests.get(f'{cls.__url()}&feed=gc&game_id={game_id}&tab=gamesummary')
 
         if response.status_code == 200:
             return response.json()['GC']['Gamesummary']
@@ -74,9 +102,12 @@ class ProspectsAPI:
 
     @classmethod
     def game_play_by_play(cls, game_id):
-        response = requests.get(f'{cls.__url}&feed=gc&game_id={game_id}&tab=pxpverbose')
+        response = requests.get(f'{cls.__url()}&feed=gc&game_id={game_id}&tab=pxpverbose')
 
         if response.status_code == 200:
             return response.json()['GC']['Pxpverbose']
 
         return []
+
+if __name__ == '__main__':
+    import code; code.interact(local=dict(globals(), **locals()))
